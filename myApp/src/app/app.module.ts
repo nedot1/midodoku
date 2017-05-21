45c1745caeecbf809e901fdb61626c5c48fec27a
nedot1 <tanja.nedovic@students.bfh.ch>
2017-05-21 12:42:16 +0200

    Vorbereitung Login

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+//import { LoginPage} from '..pages/login/login';
 import { OverviewPage} from '../pages/overview/overview';
 import { PainDocPage} from '../pages/paindoc/paindoc';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -19,6 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    //LoginPage,
     OverviewPage,
     PainDocPage,
     TabsPage,
@@ -33,6 +35,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    //LoginPage,
     OverviewPage,
     PainDocPage,
     TabsPage,
