50ae903ed13a189ce640a75fca33989802fed80b
Mujkic Ohran <ohran.mujkic@bfh.ch>
2017-05-26 09:55:03 +0200

    MODIFY INTERFERNECE

--- myApp/src/pages/interference/interference.ts
+++ myApp/src/pages/interference/interference.ts
@@ -10,12 +10,36 @@ import { HomePage} from '../home/home';
 })
 export class InterferencePage {
 
+  // ADD : START
+  // Init ngModel variable for the range sliders
+  activity : any;
+  mood : any;
+  walkingAbility : any;
+  work : any;
+  socialLife : any;
+  sleep : any;
+  vitality : any;
+  // ADD : END
+
 constructor(public navCtrl: NavController) {
 
   }
 
   pushHomePage(){
-    this.navCtrl.push(HomePage);
+
+      // ADD : START
+     // Print all values from range sliders out in console
+     console.info("Allgemeine Aktivität : " + this.activity);
+     console.info("Stimmung : " + this.mood);
+     console.info("Gehvermögen : " + this.walkingAbility);
+     console.info("Normale Arbeit (sowohl ausserhalb des Hauses als auch Hausarbeit) : " + this.work);
+     console.info("Beziehung zu anderen Menschen : " + this.socialLife);
+     console.info("Schlaf : " + this.sleep);
+     console.info("Lebensfreude : " + this.vitality);
+     // ADD : END
+
+
+    //this.navCtrl.push(HomePage);
   }
 
 }
