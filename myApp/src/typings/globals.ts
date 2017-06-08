e58d28a432fa937a260fdbec1dc9104cc7fdbebd
nedot1 <tanja.nedovic@students.bfh.ch>
2017-06-08 22:16:19 +0200

    ADD globals

---
+++ myApp/src/typings/globals.ts
@@ -0,0 +1,43 @@
+
+
+//Global Constants for BodySite
+export const SNOMED_HEAD = "69536005"
+export const SNOMED_NECK = "45048000"
+export const BACK = "35353535"
+export const SNOMED_BACK = "77568009"
+export const SNOMED_LEFT_ARM = "368208006"
+export const SNOMED_RIGHT_ARM = "368209003"
+export const SNOMED_LEFT_HAND = "85151006"
+export const SNOMED_RIGHT_HAND = "78791008"
+export const SNOMED_CHEST = "51185008"
+export const SNOMED_STOMACH = "181246003"
+export const SNOMED_LEFT_LEG = "48979004"
+export const SNOMED_RIGHT_LEG = "32696007"
+export const SNOMED_LEFT_FOOT = "22335008"
+export const SNOMED_RIGHT_FOOT = "7769000"
+
+
+
+export const SNOMED_HEAD_DISPLAY = "Kopf"
+export const SNOMED_NECK_DISPLAY = "Nacken"
+export const SNOMED_BACK_DISPLAY = "Rücken"
+export const SNOMED_LEFT_ARM_DISPLAY = "Linker Arm"
+export const SNOMED_RIGHT_ARM_DISPLAY = "Rechter Arm"
+export const SNOMED_LEFT_HAND_DISPLAY = "Linke Hand"
+export const SNOMED_RIGHT_HAND_DISPLAY = "Rechte Hand"
+export const SNOMED_CHEST_DISPLAY = "Brust"
+export const SNOMED_STOMACH_DISPLAY = "Bauch"
+export const SNOMED_LEFT_LEG_DISPLAY = "Linkes Bein"
+export const SNOMED_RIGHT_LEG_DISPLAY = "Rechtes Bein"
+export const SNOMED_LEFT_FOOT_DISPLAY = "Linker Fuss"
+export const SNOMED_RIGHT_FOOT_DISPLAY = "Rechter Fuss"
+
+//Global Constants for Pain Sensation
+export const SNOMED_BURNING_PAIN = "36349006"
+export const SNOMED_CRAMPING_PAIN = "279093005"
+export const SNOMED_CRUSHING_PAIN = "279098001"
+export const SNOMED_DULL_PAIN = "83644001"
+export const SNOMED_GNAWING_PAIN = "301371003"
+export const SNOMED_SHARP_PAIN = "8708008"
+export const SNOMED_THROBBING_PAIN = "29695002"
+export const SNOMED_TIGHTENING_PAIN = "162505000"
